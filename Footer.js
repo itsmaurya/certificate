62c436c0a97dd791d66a172ad1be248f6aa343a6
Avinash Maurya <63838996+itsmaurya@users.noreply.github.com>
2023-04-20 17:44:54 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+      <footer id="footer">
+        <h2>Avinash Maurya</h2>
+        <p><a href="https://github.com/itsmaurya" target="_blank"><i class="fa fa-github" ></i></a></p>
+        <p>
+        <a href="https://www.linkedin.com/in/mauryaavi550" target="_blank"><i className="fa fa-linkedin"></i></a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
