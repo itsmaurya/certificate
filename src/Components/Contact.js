baa977b823f0a633888d9eec2dc14e98039cb1af
Avinash Maurya <mouryaavinash550@gmail.com>
2023-04-20 17:57:57 +0530

    First Commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Contact.css"
+import Navbar from './Navbar'
+
+const Contact = () => {
+  return (
+    <div>
+        <Navbar/>
+        <section id="contact">
+          <div className="contact-container container">
+            <div className="contact-img">
+            <h2>Certificate Generator</h2>
+              {/* <img src="https://w0.peakpx.com/wallpaper/950/118/HD-wallpaper-code-coding-javascript-life-tech-technology-thumbnail.jpg" alt="" /> */}
+            </div>
+
+            <div className="form-container">
+              <h2>Contact Us</h2>
+              <input type="text" placeholder="Your Name"  style={{width:"49%"}}/>
+              <input type="email" placeholder="E-Mail" style={{width:"48%" , marginLeft:"5.6px"}}/>
+              <input type="number" placeholder="Phone" style={{width:"100%"}}/>
+              <textarea
+                cols="30"
+                rows="2"
+                placeholder="Type Your Message"
+              ></textarea>
+              <a href="#" className="btn btn-primary">Submit</a>
+            </div>
+          </div>
+        </section>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/Components/Home.js
@@ -0,0 +1,88 @@
+import React, { useState, useRef } from "react";
+import { exportComponentAsPNG } from "react-component-export-image";
+import "../App.css"
+import Certificate from "./Certificate.png";
+import Navbar from "./Navbar";
+
+const App = () => {
+    const certificateWrapper = useRef(null);
+    const [Name, setName] = useState("");
+    const [Title, setTitle] = useState("");
+    const [info, setInfo] = useState("");
+    const [imageSrc, setImageSrc] = useState(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const imageSrc = e.target.result;
+            setImageSrc(imageSrc);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="Meta">
+
+                <h1 style={{textAlign:"center", paddingBottom:"20px", fontFamily:"Josefin Sans"}}>Details Form</h1>
+
+                <p>Upload Certificate logo</p>
+                <input type="file" onChange={handleImageUpload} style={{background:"#fff", color:"#000b2c"}}/>
+
+
+                <p>Please enter Title of certificate</p>
+                <input
+                    type="text"
+                    placeholder="Please enter Title of certificate"
+                    value={Title}
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}
+                />
+                <p>Please enter your name.</p>
+                <input
+                    type="text"
+                    placeholder="Please enter your name..."
+                    value={Name}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                />
+                <p>Please enter Description of certificate</p>
+                <textarea
+                    type="text"
+                    placeholder="Please enter Description of certificate"
+                    value={info}
+                    onChange={(e) => {
+                        setInfo(e.target.value);
+                    }}
+                />
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        exportComponentAsPNG(certificateWrapper, {
+                            html2CanvasOptions: { backgroundColor: null }
+                        });
+                    }}
+                >
+                    <ion-icon name="cloud-download-outline"></ion-icon> 
+                     Download Certificate
+                </button>
+            </div>
+
+            <div id="downloadWrapper" ref={certificateWrapper}>
+                <div id="certificateWrapper" >
+                 {imageSrc && <img className="logo" src={imageSrc} alt="Uploaded Image" />}
+                    <h2>{Title}</h2>
+                    <p>{Name}</p>
+                    <p className="description">{info}</p>
+                    <img src={Certificate} alt="Certificate" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
